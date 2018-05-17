9359c618c75b042dbf27125401b08ff5e449cfe1
Northerchild <osarus-13@hotmail.com>
2018-05-16 22:21:52 -0500

    Se agrega las dependencias de estilo y se crea el footer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//Dependencias
+import JQuery from 'jquery/dist/jquery.min.js'
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import SweetAlert from 'sweetalert2/dist/sweetalert2.js'
 new Vue({
   el: '#app',
   render: h => h(App)
